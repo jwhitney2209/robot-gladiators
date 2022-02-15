f7bb7aa293f47eff64a4b9da2198b1b7f9290a30
teachergg <jwhitney@bisd.us>
2022-02-14 23:37:36 -0600

    added window prompts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a prompt for robot's name
+var playerName = window.prompt("What is your robot's name?");
+
+// Note the lack of quotation marks around playerName
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+// fight();
