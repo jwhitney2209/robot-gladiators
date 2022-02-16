77ca084393e52ec4fa301724fa29e97eca35633d
teachergg <jwhitney@bisd.us>
2022-02-15 23:15:36 -0600

    move to enemy after win

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,8 +14,9 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
+  // repeat and execute as long as enemy-robot is alive
+  while(enemyHealth > 0) {
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -68,10 +69,15 @@ var fight = function(enemyName) {
     window.alert("You need to pick a valid option. Try again!");
   }
 }
+  }
+
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  // call fight function with enemy-robot
+  fight(pickedEnemyName);
 }
 
 
